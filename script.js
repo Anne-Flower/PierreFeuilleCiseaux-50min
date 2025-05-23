2a690fac2e02d88206d3447366546f8ce7e1ab8d
Anne Flore Bernard <b.anneflore@yahoo.fr>
2025-05-23 16:35:37 +0200

    first try logic

---
+++ script.js
@@ -0,0 +1,24 @@
+const choose = ["paper", "scissor", "rock"];
+let result = [];
+let robot = choose[Math.floor(Math.random() * choose.length)];
+let player = "paper";
+
+
+function hasWin(inputPlayer, inputRobot) {
+  if (inputPlayer === "paper" && inputRobot === "scissor") {
+    return false;
+  } else if (inputPlayer === "scissor" && inputRobot === "rock") {
+    return false;
+  } else if (inputPlayer === "rock" && inputRobot === "paper") {
+    return false;
+  }
+  return true;
+}
+return result.push(hasWin(player, robot));
+
+function message() {
+  if (result[result.length - 1] === true) {
+    return "you win";
+  } else "you looose";
+}
+return message;
